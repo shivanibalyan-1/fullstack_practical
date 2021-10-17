cef5ddba656d6f6c159f1e4afdef73cef1cdd9df
shivanibalyan-1 <shivanibalyan2002@gmail.com>
2021-10-17 09:03:21 -0700

    practical added

---
+++ event.js
@@ -0,0 +1,24 @@
+function fun1(){
+    var x= document.getElementById('heading')
+    x.innerHTML="Hello World";
+    
+}
+function fun2(){
+    var x= document.getElementById('container')
+    x.style.backgroundColor = "lightpink";
+    x.style.borderRadius = "50px";
+    
+}
+function fun3(){
+    var x= document.getElementById('container')
+    x.style.backgroundColor = "lightsalmon";
+    x.style.borderRadius = "0px";
+    
+}
+function fun4(){
+    var x= document.getElementById('input')
+    x.style.Color = "lightsalmon";
+    x.style.border= "green";
+    
+}
+
